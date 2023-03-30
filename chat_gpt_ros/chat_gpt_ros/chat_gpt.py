27de676067557fa14e4104e1ccbb8eb27df85a6c
YumaMatsumura <yumapine@gmail.com>
2023-03-31 00:04:14 +0900

    update chat_gpt_ros

--- chat_gpt_ros/chat_gpt_ros/chat_gpt.py
+++ chat_gpt_ros/chat_gpt_ros/chat_gpt.py
@@ -1,31 +1,65 @@
 import openai
 
 class ChatGPT:
-    def __init__(self, api_key, max_tokens=1024, n=1, stop=None, temperature=0.5):
+    def __init__(self, api_key, system_message, model='gpt-3.5-turbo', max_total_tokens=3000, 
+                 max_tokens=1024, number_of_response=1, timeout=60, stop=None, 
+                 temperature=0.5, use_system_role=False, keep_conversation_history=True):
         self.api_key = api_key
+        self.sys_msg = system_message
+        self.model = model
+        self.max_total_tokens = max_total_tokens
         self.max_tokens = max_tokens
-        self.n = n
+        self.number_of_response = number_of_response
+        self.timeout = timeout
         self.stop = stop
         self.temperature = temperature
+        self.use_system_role = use_system_role
+        self.keep_conversation_history = keep_conversation_history
+        self.messages = []
         
     def ask_gpt(self, message):
         # APIキーの設定
         openai.api_key = self.api_key
         
-        # 応答設定
-        completion = openai.ChatCompletion.create(
-            model    = "gpt-3.5-turbo",     # モデルを選択
-            messages = [
-                {"role": "user", "content": message},
-            ],
-    
-            max_tokens  = self.max_tokens,
-            n           = self.n,
-            stop        = self.stop,
-            temperature = self.temperature,
-        )
+        # system roleの設定
+        if self.use_system_role:
+            system_message = {'role': 'system', 'content': self.sys_msg}
+            self.messages.append(system_message)
         
-        # 応答
-        response = completion.choices[0].message.content
+        # リクエストメッセージの定義
+        user_message = {'role': 'user', 'content': message}
+        self.messages.append(user_message)
         
-        return response
+        try:
+            # 応答設定
+            completion = openai.ChatCompletion.create(
+                model       = self.model,
+                messages    = self.messages,
+                max_tokens  = self.max_tokens,
+                n           = self.number_of_response,
+                timeout     = self.timeout,
+                stop        = self.stop,
+                temperature = self.temperature,
+            )
+        
+            # 応答
+            response = completion.choices[0].message.content
+        
+            # 会話履歴の保持
+            if self.keep_conversation_history:
+                gpt_message = {'role': 'assistant', 'content': response}
+                self.messages.append(gpt_message)
+            
+                if completion['usage']['total_tokens'] > self.max_total_tokens:
+                    conversation_start_index = 0
+                    if self.use_system_role:
+                        conversation_start_index = 1
+                    self.messages.pop(conversation_start_index)
+                    self.messages.pop(conversation_start_index)
+            else:
+                self.messages.clear()
+                
+            return response
+                
+        except Exception as e:
+            raise
--- chat_gpt_ros/chat_gpt_ros/chat_gpt_ros.py
+++ chat_gpt_ros/chat_gpt_ros/chat_gpt_ros.py
@@ -9,19 +9,58 @@ from chat_gpt_msgs.srv import AskChatGpt
 class ChatGPTROS(Node):
     def __init__(self):
         super().__init__('chat_gpt_node')
+        self.declare_parameter('system_message', '')
+        self.declare_parameter('model', 'gpt-3.5-turbo')
         self.declare_parameter('max_tokens', 1024)
-        self.declare_parameter('n', 1)
+        self.declare_parameter('max_total_tokens', 3000)
+        self.declare_parameter('number_of_response', 1)
+        self.declare_parameter('timeout', 60)
         self.declare_parameter('temperature', 0.5)
+        self.declare_parameter('use_system_role', False)
+        self.declare_parameter('keep_conversation_history', True)
         self.srv = self.create_service(AskChatGpt, 'ask_chat_gpt', self.ask_chat_gpt_callback)
         
-    def ask_chat_gpt_callback(self, request, response):
-        max_tokens = self.get_parameter('max_tokens').value
-        n = self.get_parameter('n').value
-        temperature = self.get_parameter('temperature').value
-        gpt = ChatGPT(api_key=os.environ["OPENAI_API_KEY"], max_tokens=max_tokens, n=n, temperature=temperature)
-        response.response_message = gpt.ask_gpt(request.request_message)
+        self.generate_chat_gpt_instance()
         
-        return response
+    def generate_chat_gpt_instance(self):
+        # Get params
+        system_message = self.get_parameter('system_message').value
+        model = self.get_parameter('model').value
+        max_tokens = self.get_parameter('max_tokens').value
+        max_total_tokens = self.get_parameter('max_total_tokens').value
+        number_of_response = self.get_parameter('number_of_response').value
+        timeout = self.get_parameter('timeout').value
+        temperature = self.get_parameter('temperature').value
+        use_system_role = self.get_parameter('use_system_role').value
+        keep_conversation_history = self.get_parameter('keep_conversation_history').value
+        
+        # Generate instance
+        self.gpt = ChatGPT(api_key=os.environ['OPENAI_API_KEY'], 
+                      system_message=system_message,
+                      model=model,
+                      max_total_tokens=max_total_tokens,
+                      max_tokens=max_tokens, 
+                      number_of_response=number_of_response, 
+                      timeout=timeout,
+                      temperature=temperature,
+                      use_system_role=use_system_role,
+                      keep_conversation_history=keep_conversation_history)
+                      
+                      
+    def ask_chat_gpt_callback(self, request, response):
+        try:
+            # Request to chatGPT
+            response.response_message = self.gpt.ask_gpt(request.request_message)
+            response.result = True
+            self.get_logger().info("ChatGPT Output: " + response.response_message)
+            
+            return response
+            
+        except Exception as e:
+            self.get_logger().error('chat_gpt_ros has failed %r' & (e,))
+            response.result = False
+            
+            return response
         
 def main(args=None):
     rclpy.init(args=args)
